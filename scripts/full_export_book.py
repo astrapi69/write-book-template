5538c87877b3312f52cec224baf712994c6679bf
Asterios Raptis <asteri.raptis@gmail.com>
2025-07-26 15:22:26 +0200

    chore(deps): add `toml` for TOML read/write support in pyproject.toml
    - Added `toml` via Poetry to enable full read/write access to TOML files
    - Justified choice over `tomli`, which supports read-only access only
    - Updated script to use `Path` for `metadata.yaml` access
    - Removed legacy `create_project_structure.sh` as it's now obsolete

--- scripts/full_export_book.py
+++ scripts/full_export_book.py
@@ -14,7 +14,6 @@ BOOK_DIR = "./manuscript"                       # Location of markdown files org
 OUTPUT_DIR = "./output"                         # Output directory for compiled formats
 BACKUP_DIR = "./output_backup"                  # Backup location for previous output
 OUTPUT_FILE = "book"                            # Base name for the output files #TODO replace with your data
-METADATA_FILE = "config/metadata.yaml"          # YAML file for Pandoc metadata (title, author, etc.)
 LOG_FILE = "export.log"                         # Log file for script and Pandoc output/errors
 
 # Paths to supporting scripts
@@ -22,6 +21,9 @@ SCRIPT_DIR = "./scripts"
 ABSOLUTE_SCRIPT = os.path.join(SCRIPT_DIR, "convert_to_absolute.py")     # Script to convert relative links to absolute
 RELATIVE_SCRIPT = os.path.join(SCRIPT_DIR, "convert_to_relative.py")     # Script to revert absolute links back to relative
 IMG_SCRIPT = os.path.join(SCRIPT_DIR, "convert_img_tags.py")             # Script to modify image tag styles if needed
+
+CONFIG_DIR = "./config"
+METADATA_FILE =  Path(CONFIG_DIR) / "metadata.yaml"     # YAML file for Pandoc metadata (title, author, etc.)
 
 # Supported output formats and their corresponding Pandoc targets
 FORMATS = {
@@ -50,12 +52,13 @@ DEFAULT_SECTION_ORDER = [
 
 def get_metadata_language():
     """Read and return the 'lang' field from metadata.yaml if present, else return None"""
-    if not os.path.exists(METADATA_FILE):
+    if not METADATA_FILE.exists():
+        print(f"⚠️ Metadata file not found at: {METADATA_FILE}")
         return None
-    with open(METADATA_FILE, "r", encoding="utf-8") as f:
+    with METADATA_FILE.open("r", encoding="utf-8") as f:
         try:
             metadata = yaml.safe_load(f)
-            return metadata.get("lang")
+            return metadata.get("language")
         except yaml.YAMLError as e:
             print(f"⚠️ Failed to parse {METADATA_FILE}: {e}")
             return None
